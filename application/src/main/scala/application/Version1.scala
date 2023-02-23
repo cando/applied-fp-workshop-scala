1e042813ae1a4311fd65f88b5e89a82d55ef7007
Pietro Martinelli <pietro.martinelli@codiceplastico.com>
2023-02-23 11:49:37 +0100

    Implement Version 1 TODO 2

--- application/src/main/scala/application/Version1.scala
+++ application/src/main/scala/application/Version1.scala
@@ -38,27 +38,54 @@ object Version1 {
   enum Command {
     case TurnLeft
     case TurnRight
-    case MoveForward(steps: Int)
-    case MoveBackward(steps: Int)
+    case MoveForward//(steps: Int)
+    case MoveBackward//(steps: Int)
   }
 
   // TODO 2: Execute all commands and accumulate final rover state
-  def executeAll(planet: Planet, rover: Rover, commands: List[Command]): Rover = ???
+  def executeAll(planet: Planet, rover: Rover, commands: List[Command]): Rover =
+    commands.foldLeft(rover)((r, cmd) => execute(planet, r, cmd))
 
   // TODO 3: Dispatch one command to a specific function
-  def execute(planet: Planet, rover: Rover, command: Command): Rover = ???
+  def execute(planet: Planet, rover: Rover, command: Command): Rover =
+    command match {
+      case Command.TurnLeft => turnLeft(rover)
+      case Command.TurnRight => turnRight(rover)
+      case Command.MoveForward => moveForward(planet, rover)
+      case Command.MoveBackward => moveBackward(planet, rover)
+    }
 
   // TODO 4: Change rover orientation
-  def turnRight(rover: Rover): Rover = ???
+  def turnRight(rover: Rover): Rover = Rover(rover.position, rover.orientation match {
+    case Orientation.North => Orientation.East
+    case Orientation.Sud => Orientation.West
+    case Orientation.West => Orientation.North
+    case Orientation.East => Orientation.Sud
+  })
 
   // TODO 5: Change rover orientation
-  def turnLeft(rover: Rover): Rover = ???
+  def turnLeft(rover: Rover): Rover = Rover(rover.position, rover.orientation match {
+    case Orientation.North => Orientation.West
+    case Orientation.Sud => Orientation.East
+    case Orientation.West => Orientation.Sud
+    case Orientation.East => Orientation.North
+  })
 
   // TODO 6: Change rover position
-  def moveForward(planet: Planet, rover: Rover): Rover = ???
+  def moveForward(planet: Planet, rover: Rover): Rover = Rover(rover.orientation match {
+    case Orientation.North => rover.position.copy(y = wrap(rover.position.y, planet.size.height,1))
+    case Orientation.Sud => rover.position.copy(y = wrap(rover.position.y, planet.size.height,-1))
+    case Orientation.West => rover.position.copy(x = wrap(rover.position.x, planet.size.width,-1))
+    case Orientation.East => rover.position.copy(x = wrap(rover.position.x, planet.size.width,1))
+  }, rover.orientation)
 
   // TODO 7: Change rover position
-  def moveBackward(planet: Planet, rover: Rover): Rover = ???
+  def moveBackward(planet: Planet, rover: Rover): Rover = Rover(rover.orientation match {
+    case Orientation.North => rover.position.copy(y = wrap(rover.position.y, planet.size.height, -1))
+    case Orientation.Sud => rover.position.copy(y = wrap(rover.position.y, planet.size.height, 1))
+    case Orientation.West => rover.position.copy(x = wrap(rover.position.x, planet.size.width, 1))
+    case Orientation.East => rover.position.copy(x = wrap(rover.position.x, planet.size.width, -1))
+  }, rover.orientation)
 
   // NOTE: utility function to get the pacman effect
   def wrap(value: Int, limit: Int, delta: Int): Int =
--- application/src/test/scala/application/Version1Tests.scala
+++ application/src/test/scala/application/Version1Tests.scala
@@ -7,66 +7,66 @@ class Version1Tests extends munit.FunSuite {
   // TODO: implements tests
 
   test("turn right command") {
-//    val planet = Planet:  5 4
-//    val rover = Rover: 0 0 N
-//    val command = Command: R
-//    val result = execute(planet, rover, command)
-//    assertEquals(result, Rover: 0 0 E)
+    val planet = Planet(Size(5,4), List())
+    val rover = Rover(Position(0, 0), Orientation.North)
+    val command = Command.TurnRight
+    val result = execute(planet, rover, command)
+    assertEquals(result, Rover(Position(0, 0), Orientation.East))
   }
 
   test("turn left command") {
-//    val planet = Planet: 5 4
-//    val rover = Rover: 0 0 N
-//    val command = Command: L
-//    val result = execute(planet, rover, command)
-//    assertEquals(result, Rover: 0 0 W)
+    val planet = Planet(Size(5, 4), List())
+    val rover = Rover(Position(0, 0), Orientation.North)
+    val command = Command.TurnLeft
+    val result = execute(planet, rover, command)
+    assertEquals(result, Rover(Position(0, 0), Orientation.West))
   }
 
   test("move forward command") {
-//    val planet = Planet: 5 4
-//    val rover = Rover: 0 1 N
-//    val command = Command: F
-//    val result = execute(planet, rover, command)
-//    assertEquals(result, Rover: 0 2 N)
+    val planet = Planet(Size(5, 4), List())
+    val rover = Rover(Position(0, 1), Orientation.North)
+    val command = Command.MoveForward
+    val result = execute(planet, rover, command)
+    assertEquals(result, Rover(Position(0, 2), Orientation.North))
   }
 
   test("move forward command, opposite orientation") {
-//    val planet = Planet: 5 4
-//    val rover = Rover: 0 1 S
-//    val command = Command: F
-//    val result = execute(planet, rover, command)
-//    assertEquals(result, Rover: 0 0 S)
+    val planet = Planet(Size(5, 4), List())
+    val rover = Rover(Position(0, 1), Orientation.Sud)
+    val command = Command.MoveForward
+    val result = execute(planet, rover, command)
+    assertEquals(result, Rover(Position(0, 0), Orientation.Sud))
   }
 
   test("move backward command") {
-//    val planet = Planet: 5 4
-//    val rover = Rover: 0 1 N
-//    val command = Command: B
-//    val result = execute(planet, rover, command)
-//    assertEquals(result, Rover: 0 0 N)
+    val planet = Planet(Size(5, 4), List())
+    val rover = Rover(Position(0, 1), Orientation.North)
+    val command = Command.MoveBackward
+    val result = execute(planet, rover, command)
+    assertEquals(result, Rover(Position(0, 0), Orientation.North))
   }
 
   test("move backward command, opposite orientation") {
-//    val planet = Planet: 5 4
-//    val rover = Rover: 0 1 S
-//    val command = Command: B
-//    val result = execute(planet, rover, command)
-//    assertEquals(result, Rover: 0 2 S)
+    val planet = Planet(Size(5, 4), List())
+    val rover = Rover(Position(0, 1), Orientation.Sud)
+    val command = Command.MoveBackward
+    val result = execute(planet, rover, command)
+    assertEquals(result, Rover(Position(0, 2), Orientation.Sud))
   }
 
   test("wrap on North") {
-//    val planet = Planet: 5 4
-//    val rover = Rover: 0 3 N
-//    val command = Command: F
-//    val result = execute(planet, rover, command)
-//    assertEquals(result, Rover: 0 0 N)
+    val planet = Planet(Size(5, 4), List())
+    val rover = Rover(Position(0, 3), Orientation.North)
+    val command = Command.MoveForward
+    val result = execute(planet, rover, command)
+    assertEquals(result, Rover(Position(0, 0), Orientation.North))
   }
 
   test("go to opposite angle") {
-//    val planet = Planet 5 4
-//    val rover = Rover: 0 0 N
-//    val commands = Commands: L F R B
-//    val result = executeAll(planet, rover, commands)
-//    assertEquals(result, Rover: 4 3 N)
+    val planet = Planet(Size(5, 4), List())
+    val rover = Rover(Position(0, 0), Orientation.North)
+    val commands = List(Command.TurnLeft, Command.MoveForward, Command.TurnRight, Command.MoveBackward)
+    val result = executeAll(planet, rover, commands)
+    assertEquals(result, Rover(Position(4, 3), Orientation.North))
   }
 }
